62d389ab356aef888b63c71ebd4f4b598a1b2edc
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-22 18:59:44 +0300

    Update sohbettemizle.js

--- komutlar/sohbettemizle.js
+++ komutlar/sohbettemizle.js
@@ -1,15 +1,15 @@
 const Discord = new require("discord.js");
 
 module.exports.run = async (bot, msg, args) => {
-    let sayi = args.join(" ");
-    if(sayi.length < 1) return msg.reply("Bir sayı girmelisin!");
-    if(sayi.length > 100) return msg.reply("deneme");
+    let sayi = args.parseInt(args[0], 10);
+    if(sayi < 1) return msg.reply("Bir sayı girmelisin!");
+    if(sayi >= 100) return msg.reply("100\'den fazla sayı giremezsin!");
 
-    msg.channel.bulkDelete(sayi).then(msg => msg.delete(sayi + 1));
+    msg.channel.bulkDelete(sayi).then(msg => msg.delete(3000));
 
     let embed = new Discord.RichEmbed()
         .setColor("RANDOM")
-        .addField("Sohbet Temizlik", "Silinen Mesaj: " + `**${sayi}**`);
+        .addField("Sohbet Temizlik", "Silinen Mesaj, " + `**${sayi}**`);
     msg.channel.send(embed);
 }
 module.exports.help = {
